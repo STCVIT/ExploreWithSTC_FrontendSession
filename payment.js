23bd84021a829ed79fd614c344a3a94a02366ce8
Megha Maitin <78341153+megh-hub@users.noreply.github.com>
2021-08-08 14:06:05 +0530

    Update payment.js

--- payment.js
+++ payment.js
@@ -3,16 +3,18 @@ const form = document.getElementById("form3");
 form.addEventListener('submit',(e)=>{
     (e).preventDefault();
 });
+let balance = 100;
 let Name = localStorage.getItem("name");
 document.getElementById("user_name").innerHTML = Name;
 function send_money(){
-    let balance= 100;
+    localStorage.getItem("balance",balance);
     let phone = document.getElementById("amount").value.trim();
     let pin = document.getElementById("passcode").value.trim();
     let trans = document.getElementById("amount").value.trim();
     if(phone!= "" && trans != "" && pin!= ""){
         if (trans<=100){
             balance=balance-trans;
+            localStorage.setItem("balance",balance);
             document.getElementById("total_balance").innerHTML = "$"+balance;
             alert("Money transferred successfully");
         }
